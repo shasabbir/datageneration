591dcc8b4141b1f09a52ec0b53b2de9168f93a9f
shasabbir <shasabbir234@gmail.com>
2023-01-28 01:08:32 +0600

    done script

---
+++ app.py
@@ -0,0 +1,35 @@
+import datetime
+import requests
+date_time_str = '2023-01-23'
+
+date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d')
+enddate = '2023-01-28'
+skipdate1 = '2023-01-24'
+skipdate2 = '2023-01-26'
+skipdate1_obj = datetime.datetime.strptime(skipdate1, '%Y-%m-%d')
+skipdate2_obj = datetime.datetime.strptime(skipdate2, '%Y-%m-%d')
+enddate_obj = datetime.datetime.strptime(enddate, '%Y-%m-%d')
+
+url = 'https://api.bscl.gov.bd/api/dayparts/save'
+#url = 'https://jsonplaceholder.typicode.com/posts'
+
+range = [30, 15]
+type = ['', 'stb', 'ott']
+while date_time_obj < enddate_obj:
+    if date_time_obj == skipdate1_obj or date_time_obj == skipdate2_obj:
+        ndate = date_time_obj + datetime.timedelta(days=1)
+        print(str(ndate)[0:10])
+        date_time_obj = ndate
+        continue
+    for r in range:
+
+        for t in type:
+
+            myobj = {"range": r, "type": t, "start": str(date_time_obj)[0:10]}
+            print(myobj)
+            x = requests.post(url, json=myobj, timeout=3600)
+            #print(x.text)
+    ndate = date_time_obj + datetime.timedelta(days=1)
+    print(str(ndate)[0:10])
+    date_time_obj = ndate
+print ("Done")
